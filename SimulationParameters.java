b437f5e0cd178b1ef87561f8c06dc6123e39c9cb
tykingston26 <KINGSTT1@my.erau.edu>
2025-02-25 15:13:08 -0500

    Update SimulationParameters.java
    Added in changes made to personal files

--- SimulationParameters.java
+++ SimulationParameters.java
@@ -2,13 +2,14 @@
 package SE300ProjectCode;
 
 import java.util.*;
-
+import java.io.*;
 public class SimulationParameters {
 	private Map<String,Double> planetParameters;
 	private Map<String,Double> spacecraftParameters;
 	
-	public void loadFromFile(String filename) {
-		
+	public Map<String,Double> loadFromFile(String filename) {
+		Map<String,Double> data = null;
+		return data;
 	}
 	
 	public void saveToFile (String filename) {

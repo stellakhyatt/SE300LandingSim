5f5d60c915a96fcfebabc9feb5268dd737a961dd
tykingston26 <KINGSTT1@my.erau.edu>
2025-03-06 15:16:38 -0500

    Update PlanetaryBody.java

--- PlanetaryBody.java
+++ PlanetaryBody.java
@@ -86,7 +86,7 @@ public class PlanetaryBody {
 	//Test Main
 	public static void main(String [] args) {
 		SimulationParameters parameters = new SimulationParameters();
-		if(!parameters.setPlanetParameters("SE300_Planet data.csv")){
+		if(!parameters.setPlanetParameters("se300_planet_data.csv")){
 			System.out.println("Planet file not found!");
 		}
 		else{
